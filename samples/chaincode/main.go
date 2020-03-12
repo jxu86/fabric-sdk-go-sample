41b0eb1bcbd415373e71f587214bb88ebf921edf
xjcun <xjcun@grgbanking.com>
2020-03-12 18:06:00 +0800

    fix bug

--- samples/chaincode/main.go
+++ samples/chaincode/main.go
@@ -24,7 +24,8 @@ func main() {
 	// defer org2Client.Close()
 
 	// Install, instantiate, invoke, query
-	Phase1(org1Client, org2Client)
+	Phase1(org1Client)
+	// Phase1(org1Client, org2Client)
 	// Install, upgrade, invoke, query
 	// Phase2(org1Client, org2Client)
 }
@@ -60,37 +61,37 @@ func Phase1(cli1) {
 	log.Println("Query chaincode success on peer0.org1")
 }
 
-func Phase2(cli1, cli2 *cli.Client) {
-	log.Println("=================== Phase 2 begin ===================")
-	defer log.Println("=================== Phase 2 end ===================")
+// func Phase2(cli1, cli2 *cli.Client) {
+// 	log.Println("=================== Phase 2 begin ===================")
+// 	defer log.Println("=================== Phase 2 end ===================")
 
-	v := "v2"
+// 	v := "v2"
 
-	// Install new version chaincode
-	if err := cli1.InstallCC(v, peer0Org1); err != nil {
-		log.Panicf("Intall chaincode error: %v", err)
-	}
-	log.Println("Chaincode has been installed on org1's peers")
+// 	// Install new version chaincode
+// 	if err := cli1.InstallCC(v, peer0Org1); err != nil {
+// 		log.Panicf("Intall chaincode error: %v", err)
+// 	}
+// 	log.Println("Chaincode has been installed on org1's peers")
 
-	if err := cli2.InstallCC(v, peer0Org2); err != nil {
-		log.Panicf("Intall chaincode error: %v", err)
-	}
-	log.Println("Chaincode has been installed on org2's peers")
+// 	if err := cli2.InstallCC(v, peer0Org2); err != nil {
+// 		log.Panicf("Intall chaincode error: %v", err)
+// 	}
+// 	log.Println("Chaincode has been installed on org2's peers")
 
-	// Upgrade chaincode only need once for each channel
-	if err := cli1.UpgradeCC(v, peer0Org1); err != nil {
-		log.Panicf("Upgrade chaincode error: %v", err)
-	}
-	log.Println("Upgrade chaincode success for channel")
+// 	// Upgrade chaincode only need once for each channel
+// 	if err := cli1.UpgradeCC(v, peer0Org1); err != nil {
+// 		log.Panicf("Upgrade chaincode error: %v", err)
+// 	}
+// 	log.Println("Upgrade chaincode success for channel")
 
-	if _, err := cli1.InvokeCC([]string{"peer0.org1.example.com",
-		"peer0.org2.example.com"}); err != nil {
-		log.Panicf("Invoke chaincode error: %v", err)
-	}
-	log.Println("Invoke chaincode success")
+// 	if _, err := cli1.InvokeCC([]string{"peer0.org1.example.com",
+// 		"peer0.org2.example.com"}); err != nil {
+// 		log.Panicf("Invoke chaincode error: %v", err)
+// 	}
+// 	log.Println("Invoke chaincode success")
 
-	if err := cli1.QueryCC("peer0.org2.example.com", "a"); err != nil {
-		log.Panicf("Query chaincode error: %v", err)
-	}
-	log.Println("Query chaincode success on peer0.org2")
-}
+// 	if err := cli1.QueryCC("peer0.org2.example.com", "a"); err != nil {
+// 		log.Panicf("Query chaincode error: %v", err)
+// 	}
+// 	log.Println("Query chaincode success on peer0.org2")
+// }
